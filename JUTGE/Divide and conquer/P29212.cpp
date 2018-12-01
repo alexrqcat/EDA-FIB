c7620d38e955c0f4b27396aed95b6d748dfb5a64
alexrqcat <32525836+alexrqcat@users.noreply.github.com>
2018-12-01 16:22:35 +0100

    Create P29212.cpp

---
+++ JUTGE/Divide and conquer/P29212.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int exp_modular(int n, int k, int m) {
+  if (k == 0) return 1;
+  else {
+    int aux = exp_modular(n, k/2, m) % m;
+    if (k%2 == 0) return (aux*aux) % m;
+    else return (((aux*aux) % m) * n) % m;
+    }
+}
+int main() {
+    int n,k,m;
+    while(cin >> n >> k >> m)
+      cout << exp_modular(n, k, m) << endl;
+}
+
+//Alex Rubio i Quintana
