2cce0117f00a91b3911c0d448511ca374ed203ff
alexrqcat <32525836+alexrqcat@users.noreply.github.com>
2019-01-16 01:46:42 +0100

    Update P60796.cpp

--- JUTGE/Graph algorithms/P60796.cpp
+++ JUTGE/Graph algorithms/P60796.cpp
@@ -1,78 +1,65 @@
 #include <iostream>
-#include <vector>
 #include <queue>
+#include <vector>
 
 using namespace std;
 
-typedef vector<char> Fila;
-typedef vector<Fila> Graph;
-typedef vector<bool> Fila_vis;
-typedef vector <Fila_vis> Mat_vis;
+typedef  vector<vector<char>> Graph;
 
-int bfs_dist(const Graph& G, const int f, const int c){
-  Mat_vis vis (G.size(),Fila_vis(G[0].size(),false));
-  queue <pair <int,int> > Q;
-  queue<int> distQ;
-  Q.push(make_pair(f,c));
-  distQ.push(0); //distancia inicial
-  vis[f][c] = true;
-  while(!Q.empty()){
-    pair<int,int> act = Q.front();
-    int x = act.first;
-    int y = act.second;
-    int dist = distQ.front()+1; //la nova distancia minima
-    //Casella superior
-    if(y > 0 and not vis[x][y-1]  and G[x][y-1] != 'X'){
-      if(G[x][y-1] == 't') return dist;
-      Q.push(make_pair(x,y-1));
-      distQ.push(dist);
-      vis[x][y-1] = true;
+int distancia_tresor(int fila, int col, const Graph& G){
+  queue<pair<int,int>> Q;
+  vector<vector<int>> dis(fila+1, vector<int> (col+1,-1));
+  int dist = 0;
+  Q.push(make_pair(fila,col));
+  while(not Q.empty()){
+    pair<int,int> pos = Q.front();
+    Q.pop();
+    int f,c;
+    f = pos.first;
+    c = pos.second;
+    dist = dis[f][c];
+    cerr << "xivato1" << endl;
+    if(f < fila and dis[f+1][c] == -1 and G[f+1][c] != 'X'){
+      if(G[f+1][c] == 't') return dist;
+      Q.push(make_pair(f+1,c));
+      dis[f+1][c] = ++dist;
+      cerr << "xivato2" << endl;
     }
-    //Casella inferior
-    if(y < G[0].size()-1 and not vis[x][y+1] and G[x][y+1] != 'X'){
-      if(G[x][y+1] == 't') return dist;
-      Q.push(make_pair(x,y+1));
-      distQ.push(dist);
-      vis[x][y+1] = true;
+    else if(f > 0 and dis[f-1][c] == -1 and G[f-1][c] != 'X'){
+      if(G[f-1][c] == 't') return dist;
+      Q.push(make_pair(f-1,c));
+      dis[f-1][c] = ++dist;
+      cerr << "xivato3" << endl;
     }
-    //Casella esquerra
-    if(x > 0 and not vis[x-1][y] and G[x-1][y] != 'X'){
-      if(G[x-1][y] == 't') return dist;
-      Q.push(make_pair(x-1,y));
-      distQ.push(dist);
-      vis[x-1][y] = true;
+    else if(c > 0 and dis[f][c-1] == -1 and G[f][c-1] != 'X'){
+      if(G[f][c-1] == 't') return dist;
+      Q.push(make_pair(f,c-1));
+      dis[f][c-1] = ++dist;
+      cerr << "xivato4" << endl;
     }
-    //Casella dreta
-    if(x < G.size()-1 and not vis[x+1][y] and G[x+1][y] != 'X'){
-      if(G[x+1][y] == 't') return dist;
-      Q.push(make_pair(x+1,y));
-      distQ.push(dist);
-      vis[x+1][y] = true;
+    else if(c < col and dis[f][c+1] == -1 and G[f][c+1] != 'X'){
+      if(G[f][c+1] == 't') return dist;
+      Q.push(make_pair(f,c+1));
+      dis[f][c+1] = ++dist;
+      cerr << "xivato5" << endl;
     }
-    Q.pop(); //ens carreguem la casella visitada
-    distQ.pop(); //ens carreguem la distancia anterior
+    cerr << "xivato6" << endl;
   }
   return -1;
 }
-void llegir_matriu(Graph& G, int n, int m){
-  for(int i = 0; i < n; ++i){ //llegim matriu
-    for(int j = 0; j < m; ++j){
-      char c;
-      cin >> c;
-      G[i][j] = c;
+
+int main(){
+  int n, m;
+  cin >> n >> m;
+  char a;
+  Graph mapa(n,vector<char>(m));
+  for(int i = 0; i < n; ++i){
+    for(int j = 0; j < m ; ++j){
+      cin >> a;
+      mapa[i][j] = a;
     }
   }
-}
-int main(){
-  int n,m;
-  cin >> n >> m;
-  Graph G(n,Fila(m));
-  llegir_matriu(G,n,m);
   int f,c;
   cin >> f >> c;
-  int dist = bfs_dist(G,f-1,c-1);
-  if(dist == -1) cout <<"no es pot arribar a cap tresor" << endl;
-  else cout <<"distancia minima: "<< dist << endl;
+  cout << distancia_tresor(f-1,c-1,mapa) << endl;
 }
-
-//Alex Rubio i Quintana
